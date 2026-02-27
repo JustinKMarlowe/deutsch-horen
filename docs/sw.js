017b0d64bea387d443bcc146e3ea723c91487ef2
JustinKMarlowe <justinkmarlowe@gmail.com>
2026-02-26 18:48:15 -0600

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'dh-v9';
+const STATIC = [
+  '/deutsch-horen/',
+  '/deutsch-horen/index.html',
+  '/deutsch-horen/manifest.json',
+  '/deutsch-horen/assets/index-1437985a.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
