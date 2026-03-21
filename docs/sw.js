9fa9756fcd6ddfe4a0e77b90421513bdf1f6bd00
JustinKMarlowe <justinkmarlowe@gmail.com>
2026-03-20 23:09:13 -0500

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'dh-v13';
+const STATIC = [
+  '/deutsch-horen/',
+  '/deutsch-horen/index.html',
+  '/deutsch-horen/manifest.json',
+  '/deutsch-horen/assets/index.js'
+];
+const CDN = [
+  'https://unpkg.com/react@18.3.1/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([...STATIC, ...CDN]))
+  );
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
